b117c6acbf32be4c9ab94d639e0e84adba612749
涂福 <2581047041@qq.com>
2022-02-28 17:12:27 +0800

    .

---
+++ frphp/common/Config.php
@@ -0,0 +1,54 @@
+<?php
+
+// +----------------------------------------------------------------------
+// | frphp { a friendly PHP Framework }
+// +----------------------------------------------------------------------
+// | Copyright (c) 2018-2099 http://frphp.jizhicms.com All rights reserved.
+// +----------------------------------------------------------------------
+// | Author: 留恋风 <2581047041@qq.com>
+// +----------------------------------------------------------------------
+// | Date：2018/02
+// +----------------------------------------------------------------------
+
+
+
+/**
+ * 核心公共配置
+ */
+ 
+
+defined('APP_PATH') or exit();
+
+return array(
+	/*系统设定*/
+	'Tpl_style'			=>	 APP_PATH.'public',//公共静态文件
+	'Tpl_common'   		=>   'common',//模板公共目录Home\View\common
+	'APP_HOME'     		=>   'app',//默认前台目录
+	'APP_URL'     		=>   APP_PATH.'index.php',//默认前台入口
+	'HOME_CONTROLLER'   =>   'Controller',//默认控制器文件目录
+	'HOME_MODEL'   		=>   'Model',//默认模型文件目录
+	'HOME_VIEW'     	=>   'View',//默认模板文件目录
+	'PC_VIEW'        	=>   'pc',//默认PC模板目录
+	'MOBILE_VIEW'       =>   'wap',//默认手机模板目录
+	'Tpl_template'		=>	 '',//默认模板目录-二级目录-多端口配置
+	'OPEN_MOBILE'	    =>	 true,//默认开启手机端
+	'File_TXT'    		=>	 '.html',//默认模板后缀名
+	'SessionTime'		=>	 3600,//默认缓存时间
+	'APP_DEBUG'			=>	 false,//关闭调试
+	'StopLog'			=>	 false,//关闭事件日志
+	'DefaultController'	=>	 'Index',//默认控制器
+	'DefaultAction'		=>	 'index',//默认方法
+	'open_url_route'	=>	 true,//开启自定义路由
+	'open_redis_session'=>	 false,//开启redis缓存session
+	'Cache_Path'		=>	 APP_PATH.'cache',//缓存目录
+	'Session_Path'		=>	 APP_PATH.'cache/tmp',//session存储目录
+	'APP_LANG'			=>	 'zh_cn',//默认当前语言，zh_cn中文简体，其他自定义
+	'APP_LANG_REQUREST'	=>	 'l',//语言包接收参数（小写的L）
+    'ROOT'				=>	 '/',//根目录路径
+
+
+
+
+
+
+);
---
+++ frphp/db/Sqlite.php
@@ -0,0 +1,127 @@
+<?php
+
+// +----------------------------------------------------------------------
+// | frphp { a friendly PHP Framework }
+// +----------------------------------------------------------------------
+// | Copyright (c) 2018-2099 http://frphp.jizhicms.com All rights reserved.
+// +----------------------------------------------------------------------
+// | Author: 留恋风 <2581047041@qq.com>
+// +----------------------------------------------------------------------
+// | Date：2022/02/28
+// +----------------------------------------------------------------------
+
+
+namespace frphp\db;
+
+use PDO;
+use PDOException;
+use PDOStatement;
+
+class Sqlite { 
+
+    private static $instance = false;
+    private $pdo;
+    private $Statement;
+    private $arrSql;
+
+    public function __construct(){ 
+        class_exists('PDO') or exit("请在php.ini里面开启php_pdo扩展！");
+        extension_loaded('pdo_sqlite') or exit("请在php.ini里面开启pdo_sqlite扩展！");
+        $sqlite = APP_PATH.DB_PATH.'/'.DB_NAME.'.db';
+        if(file_exists($sqlite)){ 
+            try {
+                $this->pdo = new PDO('sqlite:'.$sqlite); 
+            } catch (PDOException $e) {
+                exit('<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />数据库无法链接，如果您是第一次使用，请先配置数据库！');
+            }
+            
+        }else{ 
+            exit('<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />数据库无法链接，如果您是第一次使用，请先配置数据库！');
+
+        } 
+
+    } 
+
+    public static function getInstance(){
+        if(self::$instance===false){
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
+ 
+        //执行 SQL 语句，返回PDOStatement对象,可以理解为结果集
+    public function query($sql){
+        $this->arrSql[] = $sql;
+        $this->Statement = $this->pdo->query($sql);
+      //  $this->Statement->setFetchMode(PDO::FETCH_NUM); 
+        if ($this->Statement) {
+            return $this;
+        }else{
+            $msg = $this->pdo->errorInfo();
+            if($msg[2]){
+                $log_name = date('Y-m-d-H-i-s-').time();
+				register_log('数据库错误：' . $msg[2] . end($this->arrSql),$log_name);
+				exit;
+            }
+			
+        }
+    }
+    
+    
+    //执行SQL语句返回数组
+    public function getArray($sql){
+
+        if(!$result = $this->query($sql))return array();
+        $rows = array();
+        while($rows[] = $this->Statement->fetch(PDO::FETCH_ASSOC)){}
+        $this->Statement=null;
+        array_pop($rows);
+        return $rows;
+    }
+    
+    //执行一条 SQL 语句，并返回受影响的行数
+    public function exec($sql)
+    {
+        $this->arrSql[] = $sql;
+        $n = $this->pdo->exec($sql);
+        $msg = $this->pdo->errorInfo();
+        
+        if(!$n){
+            $msg = $this->pdo->errorInfo();
+            if($msg[2]){
+				$log_name = date('Y-m-d-H-i-s-').time();
+				register_log('数据库错误：' . $msg[2] . end($this->arrSql),$log_name);
+				exit;
+				
+			} 
+        }
+        return $n;
+    }
+    
+    //获取插入影响行数
+    public function lastInsertId(){
+        return $this->pdo->lastInsertId();
+    }
+    
+    //获取表信息
+    public function getTable($table){ 
+        $stmt = $this->getArray("pragma table_info({$table})");
+        if(!$stmt){
+            $msg = $this->pdo->errorInfo();
+            if($msg[2]){
+				$log_name = date('Y-m-d-H-i-s-').time();
+				register_log('数据库错误：' . $msg[2] . end($this->arrSql),$log_name);
+				exit;
+			}
+        }
+        $rows=array();
+        foreach ($stmt as $key => $value) {
+             $rows[$value['name']] = null;
+        }
+
+        return $rows;
+    }
+
+} 
+
+?>
